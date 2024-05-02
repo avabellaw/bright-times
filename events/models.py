dfbd53fb02efe474e8d4ee223fc1f3ede1c14c51
avabellaw <70448139+avabellaw@users.noreply.github.com>
2024-05-02 05:09:20 +0100

    Create address model

--- events/models.py
+++ events/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django_countries.fields import CountryField
 
-# Create your models here.
+
+class Address(models.Model):
+    class Meta:
+        verbose_name_plural = "Addresses"
+
+    full_name = models.CharField(blank=False, null=False, max_length=70)
+    street_address1 = models.CharField(blank=False, null=False,
+                                       max_length=40)
+    street_address2 = models.CharField(blank=True, null=True,
+                                       max_length=40)
+    city = models.CharField(blank=False, null=False, max_length=100)
+    postcode = models.CharField(blank=False, null=False, max_length=20)
+    country = CountryField(blank_label="(select country)",
+                           blank=False, null=False)
+    county = models.CharField(blank=False, null=False, max_length=100)
+
+    def __str__(self):
+        return f'{self.full_name}, {self.street_address1}, \
+                {self.city}, {self.postcode}, {self.country}'
