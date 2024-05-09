5292863000d42f9cfede291addde86df0d995718
avabellaw <70448139+avabellaw@users.noreply.github.com>
2024-05-09 16:33:55 +0100

    Add choose_or_create_venue template and view (#8)

--- events/urls.py
+++ events/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.events, name='events'),
     path('create/', views.create_event, name='create_event'),
+    path('choose-or-create-venue/', views.choose_or_create_venue,
+         name='choose_or_create_venue'),
 ]
--- events/views.py
+++ events/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import AddressForm, VenueForm
 from django.contrib.auth.decorators import login_required
-from .models import Event
+from .models import Event, Venue, VenueManager
 from django.contrib import messages
 
 
@@ -19,12 +19,31 @@ def events(request):
 
 
 @login_required
+def choose_or_create_venue(request):
+    # Venues associated with the current user/venue manager
+    venues = Venue.objects.filter(managers=request.user)
+
+    address_form = AddressForm()
+    venue_form = VenueForm()
+
+    template = 'events/venues/choose-or-create-venue.html'
+
+    context = {
+        'venues': venues,
+        'address_form': address_form,
+        'venue_form': venue_form,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
 def create_event(request):
     address_form = AddressForm()
     venue_form = VenueForm()
 
     # Temporary - use create venue template
-    template = 'events/create_venue.html'
+    template = 'events/event-creation/first-stage.html'
 
     context = {
         'address_form': address_form,
