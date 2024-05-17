5b9a14a777dc7e69e2a7afd2617db4b5c6596e47
avabellaw <70448139+avabellaw@users.noreply.github.com>
2024-05-18 00:23:41 +0100

    Use existing address model for user_profile address

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,18 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django_countries.fields import CountryField
+from events.models import Address
 
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=50, blank=True, null=True)
-    last_name = models.CharField(max_length=50, blank=True, null=True)
-    street_address1 = models.CharField(blank=True, null=True,
-                                       max_length=40)
-    street_address2 = models.CharField(blank=True, null=True,
-                                       max_length=40)
-    city = models.CharField(blank=True, null=True, max_length=100)
-    postcode = models.CharField(blank=True, null=True, max_length=20)
-    county = models.CharField(blank=True, null=True, max_length=100)
-    country = CountryField(blank_label="(select country)",
-                           blank=True, null=True)
+    address = models.OneToOneField(Address, null=True,
+                                   on_delete=models.CASCADE)
