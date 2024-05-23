33283209df5d7abaf7538a8602c84d03974653ee
avabellaw <70448139+avabellaw@users.noreply.github.com>
2024-05-23 23:21:57 +0100

    Add has_events to context processor (#21)

--- utils/context_processors.py
+++ utils/context_processors.py
@@ -1,4 +1,4 @@
-from events.models import VenueManager
+from events.models import VenueManager, Event
 
 
 def user_context(request):
@@ -6,6 +6,9 @@ def user_context(request):
     if user.is_authenticated:
         manager_objects = VenueManager.objects.filter(user=user)
         user.is_venue_manager = True if len(manager_objects) > 0 else False
+
+        user_events = Event.objects.filter(managers=user)
+        user.has_events = True if len(user_events) > 0 else False
 
         return {
             'user': user,
