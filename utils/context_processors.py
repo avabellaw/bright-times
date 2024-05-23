65510aaef884e3efd6a06258684a2637c7daf5c3
avabellaw <70448139+avabellaw@users.noreply.github.com>
2024-05-23 23:27:48 +0100

    Fix and refractor context processor

--- utils/context_processors.py
+++ utils/context_processors.py
@@ -5,10 +5,10 @@ def user_context(request):
     user = request.user
     if user.is_authenticated:
         manager_objects = VenueManager.objects.filter(user=user)
-        user.is_venue_manager = True if len(manager_objects) > 0 else False
+        user.is_venue_manager = len(manager_objects) > 0
 
-        user_events = Event.objects.filter(managers=user)
-        user.has_events = True if len(user_events) > 0 else False
+        user_events = Event.objects.filter(venue__managers=user)
+        user.has_events = len(user_events) > 0
 
         return {
             'user': user,
