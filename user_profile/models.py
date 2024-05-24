437e8700a03a5036882f4d2b2973d72e0bb7003f
avabellaw <70448139+avabellaw@users.noreply.github.com>
2024-05-24 19:43:23 +0100

    Add is_email_verified method to user_profile model

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from events.models import Address
+from allauth.account.admin import EmailAddress
 
 
 class UserProfile(models.Model):
@@ -8,3 +9,11 @@ class UserProfile(models.Model):
     first_name = models.CharField(max_length=50, blank=True, null=True)
     address = models.OneToOneField(Address, null=True,
                                    on_delete=models.CASCADE)
+
+    def is_email_verified(self):
+        """Return whether email has been verified.
+
+        From stackoverflow post [https://stackoverflow.com/questions/54467321/how-to-tell-if-users-email-address-has-been-verified-using-django-allauth-res]
+        """
+        return EmailAddress.objects.filter(user=self.user,
+                                           verified=True).exists()
