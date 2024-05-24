705f614cd66f167a8fe31367f45dd601a0ed3d48
avabellaw <70448139+avabellaw@users.noreply.github.com>
2024-05-24 19:43:42 +0100

    Attach user_profile to user in context proccessor

--- utils/context_processors.py
+++ utils/context_processors.py
@@ -1,4 +1,5 @@
 from events.models import VenueManager, Event
+from user_profile.models import UserProfile
 
 
 def user_context(request):
@@ -9,6 +10,7 @@ def user_context(request):
 
         user_events = Event.objects.filter(venue__managers=user)
         user.has_events = len(user_events) > 0
+        user.profile = UserProfile.objects.get(user=user)
 
         return {
             'user': user,
