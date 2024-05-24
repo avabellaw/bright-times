64eba72044367f1435b823e30a42ece54ddab776
avabellaw <70448139+avabellaw@users.noreply.github.com>
2024-05-24 02:49:52 +0100

    feat: Add event management (#21)

--- management/urls.py
+++ management/urls.py
@@ -2,11 +2,18 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Venue managament urls
     path('venue-management/', views.venue_management, name='venue-management'),
-    
+
     path('venue-management/venue-detail/<int:venue_id>',
          views.venue_detail, name='venue-management-detail'),
-    
+
     path('venue-management/delete-venue/<int:venue_id>',
          views.delete_venue, name='delete-venue'),
+
+    # Event management urls
+    path('event-management/', views.event_management, name='event-management'),
+
+    path('event-management/event-detail/<int:event_id>',
+         views.event_detail, name='event-management-detail'),
 ]
--- management/views.py
+++ management/views.py
@@ -2,14 +2,15 @@ from django.shortcuts import render
 from templates.includes.decorators import (login_required_message,
                                            must_be_venue_manager)
 
-from events.models import Venue
-from events.forms import VenueForm, AddressForm
+from events.models import Venue, Event
+from events.forms import VenueForm, AddressForm, EventForm
 from django.urls import reverse
 from django.shortcuts import redirect
 from django.conf import settings
 from .helpers import is_user_manager_of_venue
 
 ToastMessage = settings.TOAST_MESSAGE
+
 
 @login_required_message
 @must_be_venue_manager
@@ -67,3 +68,49 @@ def delete_venue(request, venue_id):
     else:
         ToastMessage.cannot_delete_venue_not_manager(request)
     return redirect(reverse('venue-management'))
+
+
+@login_required_message
+@must_be_venue_manager
+def event_management(request):
+    events = Event.objects.filter(venue__managers=request.user)
+
+    template = 'management/event/event-management.html'
+
+    context = {
+        'events': events,
+    }
+
+    return render(request, template, context)
+
+
+@login_required_message
+@must_be_venue_manager
+def event_detail(request, event_id):
+    event = Event.objects.get(pk=event_id)
+
+    if request.POST:
+        event_form = EventForm(request.POST, instance=event)
+        address_form = AddressForm(request.POST, instance=event.address)
+
+        if event_form.is_valid() and address_form.is_valid():
+            event = event_form.save(commit=False)
+            address = address_form.save()
+            event.address = address
+            event.save()
+    else:
+        event_form = EventForm(instance=event)
+        address_form = AddressForm(instance=event.address)
+
+    event_form.make_read_only()
+    address_form.make_read_only()
+    template = 'management/event/event-detail.html'
+
+    context = {
+        'event': event,
+        'event_form': event_form,
+        'address_form': address_form,
+        'breadcrumbs': [{'name': event.name}]
+    }
+
+    return render(request, template, context)
