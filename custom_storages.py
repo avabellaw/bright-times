c90054a0492aa016cbbf08c64ae5e7c80c34d61e
avabellaw <70448139+avabellaw@users.noreply.github.com>
2024-05-30 17:03:54 +0100

    prod: Add custom_storages.py file for static/media dirs

---
+++ custom_storages.py
@@ -0,0 +1,12 @@
+# From walkthrough prject. Tells django where to get and store media/static
+# files
+from django.conf import settings
+from storages.backends.s3boto3 import S3Boto3Storage
+
+
+class StaticStorage(S3Boto3Storage):
+    location = settings.STATICFILES_LOCATION
+
+
+class MediaStorage(S3Boto3Storage):
+    location = settings.MEDIAFILES_LOCATION
