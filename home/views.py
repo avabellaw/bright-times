16183b94d6f6203035ef5cb3ce2a5aa1c57d8606
avabellaw <70448139+avabellaw@users.noreply.github.com>
2024-06-01 19:37:04 +0100

    Add exclude if ticket_end_date_time in past (#47)
    In coming up section on homepage

--- home/views.py
+++ home/views.py
@@ -5,9 +5,13 @@ from datetime import datetime, timedelta
 
 def home(request):
     """View for homepage"""
+    
+    today = datetime.today()
 
-    by_next_week = datetime.today() + timedelta(weeks=1)
-    coming_up = Event.objects.filter(start_date_time__lt=by_next_week).order_by('start_date_time')
+    by_next_week = today + timedelta(weeks=1)
+    coming_up = Event.objects.filter(
+        start_date_time__lt=by_next_week
+        ).exclude(ticket_end_date_time__lt=today).order_by('start_date_time')
 
     context = {
         'coming_up': coming_up
