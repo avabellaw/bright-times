61ded3268fb4c84bbc2d768b54ff978e126b6155
avabellaw <70448139+avabellaw@users.noreply.github.com>
2024-06-01 20:05:57 +0100

    Add event helper to get available events (#47)

--- events/helpers.py
+++ events/helpers.py
@@ -1,4 +1,7 @@
+from datetime import datetime
+
 from tickets.models import Ticket
+from .models import Event
 
 
 def get_tickets_left_for_event(event):
@@ -11,3 +14,15 @@ def add_tickets_left_to_events(events):
     for event in events:
         event.tickets_left = get_tickets_left_for_event(event)
     return events
+
+
+def get_available_events():
+    """Return a list of events that have tickets available"""
+    today = datetime.today()
+
+    avail_event_ids = [event.id
+                       for event in Event.objects.exclude(
+                           ticket_end_date_time__lt=today)
+                       if get_tickets_left_for_event(event) > 0]
+
+    return Event.objects.filter(id__in=avail_event_ids)
