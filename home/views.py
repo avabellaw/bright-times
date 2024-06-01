e6b7a657ff75942cbd315175819ca6875b482105
avabellaw <70448139+avabellaw@users.noreply.github.com>
2024-06-01 20:07:42 +0100

    Add recently added events to homepage (#47)

--- home/views.py
+++ home/views.py
@@ -1,19 +1,26 @@
 from django.shortcuts import render
 from events.models import Event
 from datetime import datetime, timedelta
+from events import helpers as event_helpers
 
 
 def home(request):
     """View for homepage"""
-    
+
     today = datetime.today()
 
     by_next_week = today + timedelta(weeks=1)
-    coming_up = Event.objects.filter(
+    past_week = today - timedelta(weeks=1)
+
+    coming_up = event_helpers.get_available_events.filter(
         start_date_time__lt=by_next_week
-        ).exclude(ticket_end_date_time__lt=today).order_by('start_date_time')
+        ).order_by('start_date_time')
+
+    recently_added = event_helpers.get_available_events().filter(
+        created_on__gt=past_week).order_by('-created_on')
 
     context = {
-        'coming_up': coming_up
+        'coming_up': coming_up,
+        'recently_added': recently_added,
     }
     return render(request, 'home/index.html', context)
