7d808fdc3dedce11ad8d66912e9880cbf86e47ec
avabellaw <70448139+avabellaw@users.noreply.github.com>
2024-06-01 18:55:11 +0100

    Add events coming up to homepage (#47)

--- home/views.py
+++ home/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from events.models import Event
+from datetime import datetime, timedelta
 
 
 def home(request):
     """View for homepage"""
-    return render(request, 'home/index.html')
+
+    by_next_week = datetime.today() + timedelta(weeks=1)
+    coming_up = Event.objects.filter(start_date_time__lt=by_next_week).order_by('start_date_time')
+
+    context = {
+        'coming_up': coming_up
+    }
+    return render(request, 'home/index.html', context)
