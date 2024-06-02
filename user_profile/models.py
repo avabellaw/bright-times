5042988dfd38b34b5baefbcadcf384bdca951f2b
avabellaw <70448139+avabellaw@users.noreply.github.com>
2024-06-02 16:18:12 +0100

    Alter char limit to 30 on first and last name

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
-from events.models import Address
 
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=50, blank=True, null=True)
-    last_name = models.CharField(max_length=50, blank=True, null=True)
+    first_name = models.CharField(max_length=30, blank=True, null=True)
+    last_name = models.CharField(max_length=30, blank=True, null=True)
     stripe_customer_id = models.CharField(max_length=50, null=True)
