e1d22fdf33b044101b9f2d745ca427d263ae29e0
avabellaw <70448139+avabellaw@users.noreply.github.com>
2024-06-02 02:31:46 +0100

    Add functionality to update name + display email (#35)

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
 from .forms import UserProfileForm
+from django.contrib import messages
 
 
 def profile(request):
     user_form = UserProfileForm(instance=request.user.userprofile)
 
+    if request.POST:
+        user_form = UserProfileForm(request.POST,
+                                    instance=request.user.userprofile)
+        if user_form.is_valid():
+            user_form.save()
+            messages.success(request, 'Profile updated successfully.')
+
     template = 'user_profile/profile.html'
 
     context = {
-        'user_form': user_form
+        'user_form': user_form,
+        'user_email': request.user.email
     }
 
     return render(request, template, context)
