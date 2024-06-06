923a3a76c228ab5ff2df3a561db04f84191731ec
avabellaw <70448139+avabellaw@users.noreply.github.com>
2024-06-06 01:54:10 +0100

    Allow staff to edit their own events

--- management/templatetags/management.py
+++ management/templatetags/management.py
@@ -7,12 +7,7 @@ register = Library()
 
 @register.simple_tag
 def manager_role(venue_id, user):
-    try:
-        venue_manager = VenueManager.objects.get(venue_id=venue_id,
-                                                 user=user)
-    except VenueManager.DoesNotExist:
-        return None
-    return venue_manager.role
+    return get_manager_instance(venue_id, user).role
 
 
 @register.simple_tag
@@ -20,3 +15,13 @@ def is_venue_admin(venue_id, user):
     role = manager_role(venue_id, user)
 
     return True if role == 'ADMIN' or role == 'OWNER' else False
+
+
+@register.simple_tag
+def get_manager_instance(venue_id, user):
+    try:
+        venue_manager = VenueManager.objects.get(venue_id=venue_id,
+                                                 user=user)
+    except VenueManager.DoesNotExist:
+        return None
+    return venue_manager
