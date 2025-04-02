1c086666bc090868037e2c5bbb3061530f97b5bd
avabellaw <avabellawilliams@gmail.com>
2025-04-02 23:31:34 +0100

    feat: Add a demonstration date for showing events

--- events/helpers.py
+++ events/helpers.py
@@ -1,3 +1,7 @@
+import os
+
+from dateutil import parser
+
 from datetime import datetime
 
 from .models import Event
@@ -5,7 +9,7 @@ from .models import Event
 
 def get_available_events():
     """Return a list of events that have tickets available"""
-    today = datetime.today()
+    today = get_todays_date()
 
     avail_event_ids = [event.id
                        for event in Event.objects.exclude(
@@ -13,3 +17,11 @@ def get_available_events():
                        if event.get_tickets_left() > 0]
 
     return Event.objects.filter(id__in=avail_event_ids)
+
+
+def get_todays_date():
+    DEMO_DATE = os.environ.get('DEMO_DATE', '')
+    if DEMO_DATE:
+        return parser.parse(DEMO_DATE)
+    else:
+        return datetime.today()
--- home/views.py
+++ home/views.py
@@ -1,12 +1,19 @@
+import os
+
 from django.shortcuts import render
-from datetime import datetime, timedelta
+from datetime import timedelta
 from events import helpers as event_helpers
+from django.contrib import messages
 
 
 def home(request):
     """View for homepage"""
 
-    today = datetime.today()
+    today = event_helpers.get_todays_date()
+
+    if os.environ.get('DEMO_DATE', ''):
+        formatted_date = today.strftime("%d %B %Y")
+        messages.info(request, f'Using demostration date: {formatted_date}')
 
     by_next_week = today + timedelta(weeks=1)
     past_week = today - timedelta(weeks=1)
@@ -14,7 +21,7 @@ def home(request):
 
     coming_up = available_events.filter(
         start_date_time__lt=by_next_week
-        ).order_by('start_date_time')
+    ).order_by('start_date_time')
 
     recently_added = available_events.filter(
         created_on__gt=past_week).order_by('-created_on')
